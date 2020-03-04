b7f11832d34a79274b22b878e9b200dcc2235b8c
Therydusone <przem.pajak@gmail.com>
2020-03-04 16:06:25 +0100

    first event handlers

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
+
+// BUDDGET CONTROLLER
 var budgetController = (function() {
 
-    var x =23;
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            console.log(add(b))
-        }
-    }
+    // something
 
 })();
 
 
-
+// UI CONTROLLER
 var UIController = (function() {
 
         //something
 })();
 
 
-
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UiCtrl) {
+
+    var ctrlAddItem = function() {
+         // 1. Get data from input
+
+        // 2. Add item to budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate budget
+
+        // 5. Display the budget
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
