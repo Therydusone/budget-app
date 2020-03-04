f0ae4f1a32e7860b69c0f05b5095b19e26a55246
Therydusone <przem.pajak@gmail.com>
2020-03-04 17:35:05 +0100

    reading input

--- app.js
+++ app.js
@@ -10,16 +10,38 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-        //something
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+        return {
+            getInput: function() {
+                return {
+                     type: document.querySelector(DOMstrings.inputType).value,
+                     description: document.querySelector(DOMstrings.inputDescription).value,
+                     value: document.querySelector(DOMstrings.inputValue).value
+                }    
+            },
+
+            getDOMStrings: function() {
+                return DOMstrings
+            }
+        }
+
 })();
 
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UiCtrl) {
+    var DOM = UiCtrl.getDOMStrings();
 
     var ctrlAddItem = function() {
          // 1. Get data from input
-
+            var input = UiCtrl.getInput();
+            console.log(input)
         // 2. Add item to budget controller
 
         // 3. Add item to the UI
@@ -29,7 +51,7 @@ var controller = (function(budgetCtrl, UiCtrl) {
         // 5. Display the budget
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
