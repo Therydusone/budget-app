3d55b69f0d7529410a4e63bc15445c0474384901
Therydusone <przem.pajak@gmail.com>
2020-03-05 18:47:43 +0100

    add new expense or income logic

--- app.js
+++ app.js
@@ -2,8 +2,58 @@
 // BUDDGET CONTROLLER
 var budgetController = (function() {
 
-    // something
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+
+            var newItem, ID;
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            } else {
+                ID = 0;
+            }
+            // Create new item based or "inc" or "exp" type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            // Push item into data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+    testing: function() {
+        console.log(data)
+    }
+
+    };
 })();
 
 
@@ -17,20 +67,19 @@ var UIController = (function() {
         inputBtn: '.add__btn'
     }
 
-        return {
-            getInput: function() {
-                return {
-                     type: document.querySelector(DOMstrings.inputType).value,
-                     description: document.querySelector(DOMstrings.inputDescription).value,
-                     value: document.querySelector(DOMstrings.inputValue).value
-                }    
-            },
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }    
+        },
 
-            getDOMStrings: function() {
-                return DOMstrings
-            }
+        getDOMStrings: function() {
+            return DOMstrings
         }
-
+    }
 })();
 
 
@@ -48,11 +97,13 @@ var controller = (function(budgetCtrl, UiCtrl) {
     
     
     var ctrlAddItem = function() {
-         // 1. Get data from input
-            var input = UiCtrl.getInput();
-            console.log(input)
+        var input, newItem;
+        
+        // 1. Get data from input
+        input = UiCtrl.getInput();
+        console.log(input)
         // 2. Add item to budget controller
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value)
         // 3. Add item to the UI
 
         // 4. Calculate budget
